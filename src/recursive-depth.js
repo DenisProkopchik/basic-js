d54be5fe93b96d7ba4264947b2e6878653ca9de2
DenisProkopchik <prokopchik.1996@mail.ru>
2020-10-01 18:34:40 +0300

    feat: finish recursive task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr, result = 1) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    let checkArr = arr.findIndex(item => Array.isArray(item) === true);
+    if (checkArr >= 0) {
+      ++result;
+      arr = arr.flat();
+      return this.calculateDepth(arr, result);
+
+    } else {
+
+      return result;
+    }
   }
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,9 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(arr) {
-  //throw new CustomError('Not implemented');
+module.exports = function transform(/*arr*/) {
+  throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  if(!Array.isArray(arr)){
+  if (!Array.isArray(arr)) {
     return console.error(e);
   }
   let resultArr = arr.concat();
@@ -11,45 +11,45 @@ module.exports = function transform(arr) {
   let discardPrev = '--discard-prev';
   let doubleNext = '--double-next';
   let doublePrev = '--double-prev';
-  
+
   resultArr.forEach((item, index, array) => {
-    if(item === discardNext){
-        if( index === (array.length - 1)){
-          array.splice(index, 1);
-        } else {
-          (array[index+2] === doublePrev || array[index+2] === discardPrev) ? array.splice((index+2), 1) : null;
-          array.splice(index, 2);
-        }
-        
-        
-      } else if (item === discardPrev){
-        if( index === 0){
-          array.splice(index, 1);
-        } else {
-          array.splice((index - 1), 2);
-        }
-        
-      } else if (item === doubleNext){
-        if( index === (array.length - 1)){
-          array.splice(index, 1);
-        } else {
-          array.splice(index, 1, array[(index + 1)]);
-        }
-        
-      } else if (item === doublePrev){
-        if( index === 0){
-          array.splice(index, 1);
-        } else {
-          array.splice(index, 1, array[index - 1]);
-        }
-        
+    if (item === discardNext) {
+      if (index === (array.length - 1)) {
+        array.splice(index, 1);
+      } else {
+        (array[index + 2] === doublePrev || array[index + 2] === discardPrev) ? array.splice((index + 2), 1) : null;
+        array.splice(index, 2);
       }
-  
+
+
+    } else if (item === discardPrev) {
+      if (index === 0) {
+        array.splice(index, 1);
+      } else {
+        array.splice((index - 1), 2);
+      }
+
+    } else if (item === doubleNext) {
+      if (index === (array.length - 1)) {
+        array.splice(index, 1);
+      } else {
+        array.splice(index, 1, array[(index + 1)]);
+      }
+
+    } else if (item === doublePrev) {
+      if (index === 0) {
+        array.splice(index, 1);
+      } else {
+        array.splice(index, 1, array[index - 1]);
+      }
+
+    }
+
   });
-  
-  if(resultArr.length === 1){
-    (resultArr[0] === discardNext || resultArr[0] === discardPrev || resultArr[0] === doubleNext ||resultArr[0] === doublePrev) ? resultArr = [] : resultArr;
+
+  if (resultArr.length === 1) {
+    (resultArr[0] === discardNext || resultArr[0] === discardPrev || resultArr[0] === doubleNext || resultArr[0] === doublePrev) ? resultArr = [] : resultArr;
   }
-  
+
   return resultArr;
 };
